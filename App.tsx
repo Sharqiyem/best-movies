13698a9a56faf5bb27d2ad15152fc34d0df639f2
Salam <sharqiyem@yandex.ru>
2023-06-20 11:00:13 +0800

    add main screen

--- App.tsx
+++ App.tsx
@@ -1,19 +1,8 @@
+import MainScreen from './src/screens/MainScreen';
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
 
 function App(): JSX.Element {
-  return (
-    <SafeAreaView className="flex-1 bg-red-400 ">
-      <View className="flex-1 p-4">
-        <View>
-          <Text className="text-2xl h-11 text-red">Hello</Text>
-        </View>
-        <View className="mt-1 bg-red-200 flex-1 w-full">
-          <Text className="mt-2 text-lg text-blue-500">Hello</Text>
-        </View>
-      </View>
-    </SafeAreaView>
-  );
+  return <MainScreen />;
 }
 
 export default App;
